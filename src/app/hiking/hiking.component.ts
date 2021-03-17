d6a235a1ea95a4f77246af206a932dc5573bb4bf
Dan-77galaxy <daniel.hornak@gmail.com>
2021-03-17 20:46:12 +0100

    DH 003 filter drop down

--- src/app/hiking/hiking.component.ts
+++ src/app/hiking/hiking.component.ts
@@ -16,18 +16,31 @@ export class HikingComponent implements OnInit {
   ) {  }
 
     hiking = [
-    { title: "Slavkovský štít ", info: "Velmi zaujimava tura", image: "./assets/img.jpg" },
-    { title: "Krivan ", info: "Narocny stit", image: "http://www.severovychod.sk/content/images/8/8874_full.jpg" },
-    { title: "Gerlachovsky stit ", info: "Najvyssi stit", image: "https://ipravda.sk/res/2014/08/12/thumbs/gerlachovsky-stit-clanokW.jpg" }
+    { title: "Slavkovský štít ", info: "Velmi zaujimava tura", image: "./assets/img.jpg", location: "Vysoké Tatry" },
+    { title: "Krivan ", info: "Narocny stit", image: "http://www.severovychod.sk/content/images/8/8874_full.jpg", location: "Slovenský Raj" },
+    { title: "Gerlachovsky stit ", info: "Najvyssi stit", image: "https://ipravda.sk/res/2014/08/12/thumbs/gerlachovsky-stit-clanokW.jpg", location: "Nízke Tatry"}
 
   ];
+  locationFilter = [
+    "Všetko", "Vysoké Tatry","Slovenský Raj","Nízke Tatry"
+  ];
+
+  filteredHiking = [];
 
   ngOnInit() {
     this._http.get('https://jsonplaceholder.typicode.com/users')
       .subscribe((response: any) => {
-    
+        this.filteredHiking=this.hiking
       })
 
+  }
+
+  locationFilterChange(event) {
+    let filter = event.target.value;
+    this.filteredHiking = this.hiking.filter(item => item.location === filter);
+    if (filter === "Všetko") {
+      this.filteredHiking = this.hiking
+    }
   }
 
 
