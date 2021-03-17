3b29e650308fb5a9877895cda7a37173d9e0f1fd
EliteBook <EliteBook@DESKTOP-LBO78BP>
2021-03-17 19:48:55 +0100

    JS - 004 hiking items

--- src/app/hiking/hiking.component.ts
+++ src/app/hiking/hiking.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
 
 
 
@@ -9,7 +10,27 @@ import { Component } from "@angular/core";
   styleUrls: ['./hiking.component.scss']
 })
 
-export class HikingComponent {
-  hiking = [{ title: "Slavkovský štít ", info: "Velmi zaujimava tura" }];
+export class HikingComponent implements OnInit {
+  constructor(
+    private _http: HttpClient
+  ) {  }
+
+    hiking = [
+    { title: "Slavkovský štít ", info: "Velmi zaujimava tura", image: "./assets/img.jpg" },
+    { title: "Krivan ", info: "Narocny stit", image: "http://www.severovychod.sk/content/images/8/8874_full.jpg" },
+    { title: "Gerlachovsky stit ", info: "Najvyssi stit", image: "https://ipravda.sk/res/2014/08/12/thumbs/gerlachovsky-stit-clanokW.jpg" }
+
+  ];
+
+  ngOnInit() {
+    this._http.get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response: any) => {
+    
+      })
+
+  }
+
+
 
 }
+
