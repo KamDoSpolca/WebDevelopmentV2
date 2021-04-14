4b34d4121bbbfc080449ae1c7cac95f2f71dd0ff
EliteBook <EliteBook@DESKTOP-LBO78BP>
2021-04-14 20:20:03 +0200

    JS - 006 - DB connection

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+
 const router = require("./hiking.router");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 
+const connectionString = "mongodb+srv://admin:admin@cluster0.smkby.mongodb.net/Hikingapp?retryWrites=true&w=majority";
+
+
 app.use(cors());
 
 app.use("/", router);
 
+mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+  .then(() => {
+    console.log("databaza je pripojena")
+  })
+  .catch(() => {
+    console.log("databaza nie je pripojena")
+  });
 
 app.listen(port, () => {
   console.log("server is listening on port " + port)
