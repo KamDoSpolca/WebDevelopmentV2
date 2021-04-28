d91a29dd00582255f741ce4216f0ceddb1d758c1
KamDoSpolca <martinqo277@azet.sk>
2021-04-28 17:06:00 +0000

    MP - 007 - deployment fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Home/home.component';
-import { MapComponent } from './Map/map.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
