fc155e150dfbbf9049c4952b047b82c0313a7c74
EliteBook <EliteBook@DESKTOP-LBO78BP>
2021-06-30 19:52:02 +0200

    JS - 011 - AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { AttractionComponent } from './hiking/attraction/attraction.component';
 import { HikingComponent } from './hiking/hiking.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './login/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { MapComponent } from './map/map.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -13,7 +14,7 @@ const routes: Routes = [
   { path: 'map', component: MapComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'not-found', component: NotFoundComponent },
-  { path: 'hiking', component: HikingComponent },
+  { path: 'hiking', component: HikingComponent, canActivate: [AuthGuard] },
   { path: 'hiking/:id', component: AttractionComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '/not-found' }
@@ -21,6 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ src/app/login/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HikingService } from '../hiking/hiking.service';
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private _hikingService: HikingService,
+    private _router: Router
+
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    let isLoggedIn = this._hikingService.getIsLogIn()
+    if (isLoggedIn) {
+      return true
+
+    } else {
+      this._router.navigate(["/login"]) 
+
+    }
+  }
+}
